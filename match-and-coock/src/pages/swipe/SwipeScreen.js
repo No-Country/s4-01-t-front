bef0f362dbaebafde6cde5ce8a62ebe2143311ed
ezSan <ezequiel.sanchez.7391@gmail.com>
2022-10-18 18:21:45 -0300

    desktop _ toDo_ addButtonLike

--- match-and-coock/src/pages/swipe/SwipeScreen.js
+++ match-and-coock/src/pages/swipe/SwipeScreen.js
@@ -3,23 +3,35 @@ import Container from "react-bootstrap/Container";
 import styles from "./swipeScreen.module.css";
 import data from "../../mockMenues.json";
 import SwipeContainer from "../../components/swipeContainer/SwipeContainer";
-import RecipesGrid from '../../components/recipesGrid/RecipesGrid'
+import RecipesGrid from '../../components/recipesGrid/RecipesGrid';
+import ButtonsContainer from "../../components/buttons/buttonsContainer/ButtonsContainer";
 
-const { viewTitle, swipeAndLikes, rightCtn } = styles;
+const { viewTitle, swipeAndLikes, rightContent, leftContent, centerContent, recipes } = styles;
 
 function SwipeScreen() {
   const { menues } = data;
 
   return (
     <div className={swipeAndLikes}>
-      <div style={{width:'40%'}}>
-        <h2 className={viewTitle}>Deslizá y matcheá</h2>
-        <SwipeContainer menues={menues} />
+
+      <div>
+        <div>
+          <ButtonsContainer />
+        </div>
+
+        <div >
+          <h2 className={viewTitle}>Deslizá y matcheá</h2>
+          <SwipeContainer menues={menues} />
+        </div>
+
       </div>
-      <div style={{width:'60%', display:'flex', flexDirection:'column', justifyContent:'space-around'}}>
-        <h2 className={viewTitle}>Tus recetas likeadas</h2>
+
+
+      <div className={rightContent}>
+        <h2 className={viewTitle}>Tus recetas matcheadas</h2>
         <RecipesGrid menues={menues} />
-      </div> */
+      </div>
+
     </div>
 
   );
